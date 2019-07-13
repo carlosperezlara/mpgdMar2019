44de6012cc4be386fe108d6abe12cac8c8e2be39
carlosperezlara <cperez@Perezs-MBP.home>
2019-07-12 20:04:22 -0400

    upd

--- resolution.C
+++ resolution.C
@@ -1,122 +1,10 @@
-Int_t fIndex;
-Int_t fRun;
-Int_t fDet;
-Double_t fXPitch;
-Double_t fYBeat;
-Double_t fXStretch;
-
-TString fCell;
-TString fSDet;
-TString fBoard;
-TString fTech;
 TTree *tree;
-Double_t fGXmin=-10;
-Double_t fGXmax=+10;
-Double_t fGYmin=-10;
-Double_t fGYmax=+10;
-Double_t fChiMax = 2;
-
-void GetConfig() {
-
-  ifstream fin("elog/runs.dat.evt.num");
-  Int_t index = fIndex;
-  for(;;--index) {
-    fin >> fRun;
-    if(index==0) {
-      break;
-    }
-  }
-  fin.close();
-
-  double tdbl;
-  int tint;
-  fin.open("track/dat/chi2_stat.dat");
-  for(;;) {
-    fin >> tint;
-    if(!fin.good()) {
-      break;
-    }
-    if(tint==fRun) {
-      fin >> fChiMax >> tdbl;
-      fChiMax += 3*tdbl;
-      break;
-    } else {
-      fin >> tdbl >> tdbl;
-    }
-  }
-  fin.close();
-
-
-  fin.open("elog/runs.dat.evt.cel");
-  index = fIndex;
-  for(;;--index) {
-    fin >> fCell;
-    if(index==0) {
-      break;
-    }
-  }
-  fin.close();
-
-  fin.open( Form("elog/aquaconf/run_0000%d.p907",fRun) );
-  index = fDet;
-  std::string line;
-  for(;;--index) {
-    std::getline(fin,line);
-    if(index==0) {
-      break;
-    }
-    fin.clear();
-  }
-  TString tline = line;
-  fBoard = ( (TObjString*) (tline.Tokenize(" "))->At(0) )->GetString();
-  fTech = ( (TObjString*) (tline.Tokenize(" "))->At(4) )->GetString();
-  fin.close();
-
-  fin.open( Form("mapping/pitch_%s.dat",fBoard.Data()) );
-  TString cell;
-  for(;;) {
-    fin >> cell >> fXPitch;
-    if(!fin.good()) break;
-    if(fCell.Contains(cell)) {
-      break;
-    }
-  }
-  fin.close();
-
-  fin.open( Form("mapping/%s.dat",fBoard.Data()) );
-  for(;;) {
-    fin >> cell >> fXPitch >> fYBeat >> fXStretch;
-    if(!fin.good()) break;
-    if(fCell.Contains(cell)) {
-      break;
-    }
-  }
-  fin.close();
-
-  fin.open( Form("config/fiducial.%s.dat",fSDet.Data()) );
-  int run;
-  double tmp1, tmp2, tmp3, tmp4;
-  for(;;) {
-    fin >> run >> tmp1 >> tmp2 >> tmp3 >> tmp4;
-    if(!fin.good()) break;
-    if(fRun==run) { // only if found replace
-      fGXmin = tmp1;
-      fGXmax = tmp2;
-      fGYmin = tmp3;
-      fGYmax = tmp4;
-      break;
-    }
-  }
-  fin.close();
-
-}
-
+#include "GetConfig.C"
 
 void resolution(int idx=24, int det = 3 )
 {
   fIndex = idx;
   fDet = det;
-  fSDet = Form("D%d",det);
   GetConfig();
   std::cout << "Run " << fRun << endl;
   std::cout << "Cell " << fCell << endl;
@@ -423,7 +311,8 @@ void resolution(int idx=24, int det = 3 )
 		      det,fTech.Data(),fBoard.Data(),fRun) );
   std::cout << " RESULTS IN " << Form("res/D%d/data/reso_%s_%s_run%d.dat",
 				      det,fTech.Data(),fBoard.Data(),fRun) << std::endl;
-  fout << resultReso;
+  fout << dxsig = dx->GetParameter(2);
+  //fout << resultReso;
   fout << " " << fRun;
   fout << " " << fCell;
   fout << " " << fXPitch;
