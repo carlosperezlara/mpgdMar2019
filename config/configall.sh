e2c60fc554c637410650885e402836c480f50a1c
carlosperezlara <cperez@Perezs-MBP.home>
2019-07-21 19:51:04 -0400

    offset

--- config/configall.sh
+++ config/configall.sh
@@ -3,6 +3,7 @@ do
     rm D${X}.txt
     #head -$X configall.txt | tail -1 > D$X.txt.start
     #ln -s D${X}.txt.start D${X}.txt
-    cp ../D${X}.beamnew .
-    ln -s D${X}.beamnew D${X}.txt
+    #cp ../D${X}.beamnew .
+    #ln -s D${X}.beamnew D${X}.txt
+    ln -s D${X}.beamnew_off D${X}.txt
 done
