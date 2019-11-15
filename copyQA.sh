b0abb61a3e064b463fcd5b6ff3ad685702576535
carlosperezlara <cperez@Perezs-MBP.home>
2019-11-14 21:54:22 -0500

    changes

--- copyQA.sh
+++ copyQA.sh
@@ -1,4 +1,4 @@
 for X in 1 2 3 4 5 6 7 8
 do
-    scp perez@rftpexp.rhic.bnl.gov:mpgdMar2019/res/D${X}/FIDUCIAL* res/D${X}/
+    scp perez@rftpexp.rhic.bnl.gov:mpgdMar2019/res/D${X}/qaextra*pdf res/D${X}/
 done
